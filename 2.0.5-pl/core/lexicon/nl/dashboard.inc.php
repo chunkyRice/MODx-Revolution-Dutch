dd122139b0e565808412227ab7d0cbed29cae95d
Bert Oost <bert@oostdesign.nl>
2010-12-18 18:47:39 +0100

    Diverse teksten aangepast, eigenschapsets moet gewoon propertysets blijven, dat is een modx ding

--- 2.0.5-pl/core/lexicon/nl/dashboard.inc.php
+++ 2.0.5-pl/core/lexicon/nl/dashboard.inc.php
@@ -6,11 +6,11 @@
  * @package modx
  * @subpackage lexicon
  * 
- * @author MJ Hofland, veiliggesteld.nl
+ * @author Bert Oost, <bertoost85@gmail.com>
  */
 $_lang['active_users_none'] = 'Er zijn geen actieve gebruikers.';
-$_lang['add_user'] = 'Nieuwe Gebruiker';
-$_lang['create_resource'] = 'Nieuwe Resource';
+$_lang['add_user'] = 'Nieuwe gebruiker';
+$_lang['create_resource'] = 'Nieuw document';
 $_lang['dashboard'] = 'Dashboard';
 $_lang['documentation'] = 'Documentatie';
 $_lang['file_bug'] = 'Rapporteer een fout';
@@ -20,16 +20,16 @@ $_lang['modx_news'] = 'MODx Nieuws';
 $_lang['onlineusers_action'] = 'Actie';
 $_lang['onlineusers_actionid'] = 'Actie-ID';
 $_lang['onlineusers_ipaddress'] = 'Gebruiker\'s IP adres';
-$_lang['onlineusers_lasthit'] = 'Laatste Hit';
-$_lang['onlineusers_message'] = 'In deze lijst staan alle actieve gebruikers uit de laatste 20 minuten (het is nu [[+curtime]])';
+$_lang['onlineusers_lasthit'] = 'Laatste hit';
+$_lang['onlineusers_message'] = 'In deze lijst staan alle actieve gebruikers die in de laatste 20 minuten zijn ingelogd (het is nu [[+curtime]])';
 $_lang['onlineusers_title'] = 'Online gebruikers';
 $_lang['onlineusers_user'] = 'Gebruiker';
 $_lang['onlineusers_userid'] = 'Gebruiker\'s ID';
-$_lang['quicklinks'] = 'Quicklinks';
-$_lang['security_notices'] = 'Beveiligings opmerkingen';
-$_lang['view_tree'] = 'Bekijk Tree';
+$_lang['quicklinks'] = 'Snelkoppelingen';
+$_lang['security_notices'] = 'Veiligheidsaankondigingen';
+$_lang['view_tree'] = 'Bekijk structuur';
 $_lang['welcome_back'] = 'Welkom terug, [[+name]].';
-$_lang['welcome_messages'] = 'Er zijn <strong>%d</strong> berichten in je inbox, waarvan <strong>%s</strong> ongelezen.';
+$_lang['welcome_messages'] = 'Er is/zijn <strong>%d</strong> bericht(en) in jouw inbox, waarvan <strong>%s</strong> ongelezen.';
 $_lang['welcome_title'] = 'Welkom bij de MODx Content Manager';
 $_lang['yourinfo_message'] = 'Deze sectie laat informatie over jou zien:';
 $_lang['yourinfo_previous_login'] = 'Je laatste login:';
--- 2.0.5-pl/core/lexicon/nl/login.inc.php
+++ 2.0.5-pl/core/lexicon/nl/login.inc.php
@@ -17,11 +17,11 @@ $_lang['login_blocked_too_many_attempts'] = 'Je bent geblokkeerd door te veel mi
 $_lang['login_button'] = 'Inloggen';
 $_lang['login_cannot_locate_account'] = 'Het account kon niet gevonden worden. Controleer je gebruikersnaam en type het wachtwoord om het nog eens te proberen.';
 $_lang['login_copyright'] = '&copy; 2005-2010 bij het <a href="http://modxcms.com/" target="_blank">MODx Team</a>. <strong>MODx</strong>&trade; is gelicentieerd onder de GPLv2 of later.';
-$_lang['login_email_label'] = 'Account e-mail:';
+$_lang['login_email_label'] = 'E-mailadres:';
 $_lang['login_err_unknown'] = 'Er is een onbekende fout opgetreden tijdens het inloggen.';
 $_lang['login_forget_your_login'] = 'Je inlogaccount vergeten?';
 $_lang['login_hostname_error'] = 'Je hostnaam wijst niet terug naar jouw IP adres.';
-$_lang['login_message'] = 'Vul je inloggegevens in om jouw Manager sessie te starten. Je gebruikersnaam en wachtwoord zijn hoofdlettergevoelig, dus vul het met zorg in!';
+$_lang['login_message'] = 'Vul hier jouw inloggevens in om toegang te krijgen tot de Manager. Je gebruikersnaam en wachtwoord zijn hoofdlettergevoelig, dus vul het met zorg in!';
 $_lang['login_modx_support'] = '<p>Ondersteund door:</p><p><a href="http://www.modxcms.com/forums/" target="_blank"><strong>The MODx Community</strong></a></p>';
 $_lang['login_password'] = 'Wachtwoord: ';
 $_lang['login_password_reset_act_sent'] = 'Wachtwoord herstel e-mail is verstuurd. Controleer je e-mail inbox.';
