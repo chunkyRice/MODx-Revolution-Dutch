cd7ba0223cae723c8f7da5f7fb341460fcbb0e43
Bert Oost <bert@webserver.adwise.local>
2011-04-06 17:31:39 +0200

    Moved old 2.0.5-pl files into root

---
+++ core/lexicon/nl/dashboard.inc.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Welcome Page Dutch lexicon topic
+ *
+ * @language nl
+ * @package modx
+ * @subpackage lexicon
+ * 
+ * @author Bert Oost, <bertoost85@gmail.com>
+ */
+$_lang['active_users_none'] = 'Er zijn geen actieve gebruikers.';
+$_lang['add_user'] = 'Nieuwe gebruiker';
+$_lang['create_resource'] = 'Nieuw document';
+$_lang['dashboard'] = 'Dashboard';
+$_lang['documentation'] = 'Documentatie';
+$_lang['file_bug'] = 'Rapporteer een fout';
+$_lang['forums'] = 'Forums';
+$_lang['help'] = 'Help';
+$_lang['modx_news'] = 'MODx Nieuws';
+$_lang['onlineusers_action'] = 'Actie';
+$_lang['onlineusers_actionid'] = 'Actie-ID';
+$_lang['onlineusers_ipaddress'] = 'Gebruiker\'s IP adres';
+$_lang['onlineusers_lasthit'] = 'Laatste hit';
+$_lang['onlineusers_message'] = 'In deze lijst staan alle actieve gebruikers die in de laatste 20 minuten zijn ingelogd (het is nu [[+curtime]])';
+$_lang['onlineusers_title'] = 'Online gebruikers';
+$_lang['onlineusers_user'] = 'Gebruiker';
+$_lang['onlineusers_userid'] = 'Gebruiker\'s ID';
+$_lang['quicklinks'] = 'Snelkoppelingen';
+$_lang['security_notices'] = 'Veiligheidsaankondigingen';
+$_lang['view_tree'] = 'Bekijk structuur';
+$_lang['welcome_back'] = 'Welkom terug, [[+name]].';
+$_lang['welcome_messages'] = 'Er is/zijn <strong>%d</strong> bericht(en) in jouw inbox, waarvan <strong>%s</strong> ongelezen.';
+$_lang['welcome_title'] = 'Welkom bij de MODx Content Manager';
+$_lang['yourinfo_message'] = 'Deze sectie laat informatie over jou zien:';
+$_lang['yourinfo_previous_login'] = 'Je laatste login:';
+$_lang['yourinfo_title'] = 'jouw info';
+$_lang['yourinfo_total_logins'] = 'Totaal aantal logins:';
+$_lang['yourinfo_username'] = 'Je bent ingelogd als:';
